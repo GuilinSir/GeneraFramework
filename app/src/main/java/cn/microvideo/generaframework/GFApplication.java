a1e3693abf19ced4fdf7501d929901c113266c9e
guilin <308139995@qq.com>
2018-03-19 11:36:28 +0800

    first commit

---
+++ app/src/main/java/cn/microvideo/generaframework/GFApplication.java
@@ -0,0 +1,13 @@
+package cn.microvideo.generaframework;
+
+import com.jess.arms.base.BaseApplication;
+
+/**
+ * Created by Guilin on 2017/11/29. 18:09
+ * WeChat: 308139995
+ * E-Mail: 308139995@qq.com
+ */
+
+public class GFApplication extends BaseApplication {
+
+}
